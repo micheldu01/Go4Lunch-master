f908c5ca0e9b523bd2a81344d4a8066e737d25b3
michel <laurentmichelst@gmail.com>
2018-05-02 10:22:50 +0200

    CREATE CLASS USER

--- app/src/main/java/com/example/michel/go4lunch/auth/User.java
+++ app/src/main/java/com/example/michel/go4lunch/auth/User.java
@@ -1,4 +1,54 @@
 package com.example.michel.go4lunch.auth;
 
+import android.support.annotation.Nullable;
+
 public class User {
+
+    private String uid;
+    private String username;
+    private String email;
+    @Nullable private String urlPicture;
+
+    public User() {
+    }
+
+    public User(String uid, String username, String email, String urlPicture) {
+        this.uid = uid;
+        this.username = username;
+        this.email = email;
+        this.urlPicture = urlPicture;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Nullable
+    public String getUrlPicture() {
+        return urlPicture;
+    }
+
+    public void setUrlPicture(@Nullable String urlPicture) {
+        this.urlPicture = urlPicture;
+    }
 }
