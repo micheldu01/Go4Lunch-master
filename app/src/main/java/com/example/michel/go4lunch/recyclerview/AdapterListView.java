273de5561fff02421efcb3c995dc0676be175fd7
michel <laurentmichelst@gmail.com>
2018-05-15 16:56:49 +0200

    add icon for recycler list view

--- app/src/main/java/com/example/michel/go4lunch/recyclerview/AdapterListView.java
+++ app/src/main/java/com/example/michel/go4lunch/recyclerview/AdapterListView.java
@@ -1,9 +1,26 @@
 package com.example.michel.go4lunch.recyclerview;
 
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
 public class AdapterListView extends RecyclerView.Adapter {
 
 
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
---
+++ app/src/main/java/com/example/michel/go4lunch/recyclerview/ViewHolderListView.java
@@ -0,0 +1,74 @@
+package com.example.michel.go4lunch.recyclerview;
+
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class ViewHolderListView extends RecyclerView.ViewHolder {
+
+
+    // ADD VALUES
+    private TextView name;
+    private TextView adresse;
+    private TextView heuresOuverture;
+    private TextView type;
+    private TextView etoile;
+    private TextView workMates;
+    private TextView distance;
+    private TextView photo;
+
+
+    // CREATE CONSTRUCTOR WITH THE VALUES
+    public ViewHolderListView(View itemView) {
+        super(itemView);
+
+        // CALL VALUES
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
