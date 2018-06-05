57f05e0ff9dfad2e140c6b94105d6340b4195fc0
michel <laurentmichelst@gmail.com>
2018-06-05 11:19:11 +0200

    CREATE AND IMPLEMENT GOOGLE MAP SERVICE

--- app/src/main/java/com/example/michel/go4lunch/APIMaps/GoogMapService.java
+++ app/src/main/java/com/example/michel/go4lunch/APIMaps/GoogMapService.java
@@ -6,13 +6,21 @@ import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface GoogMapService {
 
 
-    // CREATE REQUEST FOR TOP STORIES API NYT
-    @GET("maps/api/place/nearbysearch/json?location=46.066667,5.316667&radius=10000&type=restaurant&key=AIzaSyBTdkUz22ebr4rMq63NRDdzCFcB9XX8Uqs")
-    Observable<GoogleAPI> getGoogleApi();
+
+    // CREATE REQUEST FOR GET NEARBY PLACE
+    @GET("maps/api/place/nearbysearch/json?location=46.066667,5.316667&radius=10000&type=restaurant")
+    Observable<GoogleApiA> getGoogleApi(@Query("key")String key);
+
+    // CREATE REQUEST FOR GET SPECIFICS VALUES FROM RESTAURANT
+    @GET("maps/api/place/details/json?")
+    Observable<GoogleAPIplaceId> getGoogleAPIplaceId(
+            @Query("key")String key,
+            @Query("placeid")String place_id);
 
 
     // USE RETROFIT FOR GET DATA
